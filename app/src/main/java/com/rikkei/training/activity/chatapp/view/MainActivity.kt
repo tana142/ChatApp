5f98bc864f725406ffad0a6b3a57dbc791b4144c
TanBD <tanbd@rikkeisoft.com>
2021-12-01 17:24:02 +0700

    design splash

--- app/src/main/java/com/rikkei/training/activity/chatapp/view/MainActivity.kt
+++ app/src/main/java/com/rikkei/training/activity/chatapp/view/MainActivity.kt
@@ -1,14 +1,15 @@
 package com.rikkei.training.activity.chatapp.view
 
-import android.opengl.Visibility
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.view.View
 import android.view.WindowManager
+import androidx.fragment.app.commit
 import com.rikkei.training.activity.chatapp.R
 import com.rikkei.training.activity.chatapp.databinding.ActivityMainBinding
+import com.rikkei.training.activity.chatapp.view.fragment.splash.SplashFragment
 
-class MainActivity : AppCompatActivity() {
+class MainActivity : AppCompatActivity(), MainInterface {
     private val binding by lazy { ActivityMainBinding.inflate(layoutInflater) }
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
@@ -17,6 +18,9 @@ class MainActivity : AppCompatActivity() {
             WindowManager.LayoutParams.FLAG_LAYOUT_NO_LIMITS,
             WindowManager.LayoutParams.FLAG_LAYOUT_NO_LIMITS
         )
+        supportFragmentManager.commit {
+            replace(R.id.container_view, SplashFragment.Instance(this@MainActivity))
+        }
 
     }
     fun showBottomNavigation(){
@@ -24,5 +28,13 @@ class MainActivity : AppCompatActivity() {
     }
     fun hideBottomNavigation(){
         binding.viewBottom.visibility = View.GONE
+    }
+
+    override fun showNavigationView() {
+        showBottomNavigation()
+    }
+
+    override fun hideNavigationView() {
+        hideBottomNavigation()
     }
 }
