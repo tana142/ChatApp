412a7c5ec37606cae4bcbb2dff4e3cbca98bc574
TanBD <tanbd@rikkeisoft.com>
2021-12-01 17:24:02 +0700

    design splash

---
+++ app/src/main/java/com/rikkei/training/activity/chatapp/view/MainInterface.kt
@@ -0,0 +1,6 @@
+package com.rikkei.training.activity.chatapp.view
+
+interface MainInterface {
+    fun showNavigationView()
+    fun hideNavigationView()
+}
--- app/src/main/java/com/rikkei/training/activity/chatapp/view/fragment/splash/SplashFragment.kt
+++ app/src/main/java/com/rikkei/training/activity/chatapp/view/fragment/splash/SplashFragment.kt
@@ -5,20 +5,30 @@ import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import androidx.fragment.app.commit
 import com.rikkei.training.activity.chatapp.R
+import com.rikkei.training.activity.chatapp.view.MainInterface
 import com.rikkei.training.activity.chatapp.view.fragment.login.LoginFragment
+import java.util.*
+import kotlin.concurrent.schedule
 
-class SplashFragment : Fragment() {
+class SplashFragment(private val mainInterface: MainInterface) : Fragment() {
 
     override fun onCreateView(
         inflater: LayoutInflater, container: ViewGroup?,
         savedInstanceState: Bundle?
     ): View? {
         // Inflate the layout for this fragment
+        mainInterface.hideNavigationView()
+        Timer("Starting", false).schedule(3000){
+            parentFragmentManager.commit {
+                replace(R.id.container_view, LoginFragment.Instance())
+            }
+        }
         return inflater.inflate(R.layout.fragment_splash, container, false)
     }
 
     companion object {
-        fun Instance() = SplashFragment()
+        fun Instance(mainInterface: MainInterface) = SplashFragment(mainInterface)
     }
 }
