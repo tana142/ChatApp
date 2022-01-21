3f4ac56b685b859cb6a1e49ab56af21b1658b945
TN14 <buidangtan01042000@gmail.com>
2022-01-21 20:05:29 +0700

    code multi languages

--- app/src/main/java/com/rikkei/training/activity/chatapp/view/fragment/profile/ProfileFragment.kt
+++ app/src/main/java/com/rikkei/training/activity/chatapp/view/fragment/profile/ProfileFragment.kt
@@ -1,5 +1,7 @@
 package com.rikkei.training.activity.chatapp.view.fragment.profile
 
+import android.app.AlertDialog
+import android.content.DialogInterface
 import android.graphics.BitmapFactory
 import android.os.Bundle
 import android.util.Base64
@@ -7,14 +9,21 @@ import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import android.widget.Toast
+import androidx.appcompat.app.AppCompatActivity
 import androidx.fragment.app.commit
 import androidx.fragment.app.viewModels
 import androidx.lifecycle.Observer
-import com.google.firebase.auth.FirebaseAuth
+import com.google.firebase.auth.ktx.auth
+import com.google.firebase.ktx.Firebase
 import com.rikkei.training.activity.chatapp.R
 import com.rikkei.training.activity.chatapp.databinding.FragmentProfileBinding
 import com.rikkei.training.activity.chatapp.view.MainInterface
+import com.rikkei.training.activity.chatapp.view.SHARE_PREF_LANG
+import com.rikkei.training.activity.chatapp.view.SHARE_PREF_NAME
+import com.rikkei.training.activity.chatapp.view.fragment.login.LoginFragment
 import com.rikkei.training.activity.chatapp.viewmodel.profile.ProfileViewModel
+import java.util.*
 
 class ProfileFragment(private val mainInterface: MainInterface) : Fragment() {
 
@@ -26,11 +35,21 @@ class ProfileFragment(private val mainInterface: MainInterface) : Fragment() {
         savedInstanceState: Bundle?
     ): View? {
         mainInterface.showNavigation()
+        val sharePref = context?.getSharedPreferences(SHARE_PREF_NAME, AppCompatActivity.MODE_PRIVATE)
+        val lang = sharePref?.getString(SHARE_PREF_LANG,"")
+        if(lang != null){
+            if(lang == "vi"){
+                binding.tvLanguages.text = context?.resources?.getString(R.string.viet_nam)
+            }
+            if(lang == "en"){
+                binding.tvLanguages.text = context?.resources?.getString(R.string.english)
+            }
+        }
 
         viewModel.user.observe(viewLifecycleOwner, Observer {
             if (it != null) {
-                binding.tvUserName.setText(it.name)
-                binding.tvUserEmail.setText(it.email)
+                binding.tvUserName.text = it.name
+                binding.tvUserEmail.text = it.email
                 val bytes: ByteArray = Base64.decode(it.avatar, Base64.DEFAULT)
                 val bitmap = BitmapFactory.decodeByteArray(bytes, 0, bytes.size)
                 binding.imageAvatarUser.setImageBitmap(bitmap)
@@ -48,9 +67,38 @@ class ProfileFragment(private val mainInterface: MainInterface) : Fragment() {
                 addToBackStack("Edit")
             }
         }
-//        binding
+        binding.viewLogout.setOnClickListener {
+            Firebase.auth.signOut()
+            parentFragmentManager.commit {
+                replace(R.id.container_view, LoginFragment.Instance(mainInterface))
+            }
+        }
 
+        binding.imageLanguagesArrow.setOnClickListener {
+            showChangeLanguagesDialog()
+        }
+        binding.tvLanguages.setOnClickListener {
+            showChangeLanguagesDialog()
+        }
         return binding.root
+    }
+    fun showChangeLanguagesDialog(){
+        val listLanguages = arrayOf("Việt Nam", "English")
+        val builder = AlertDialog.Builder(context)
+        builder.setTitle(context?.resources?.getString(R.string.choose_language))
+        builder.setSingleChoiceItems(listLanguages, -1, object: DialogInterface.OnClickListener{
+            override fun onClick(dialog: DialogInterface?, which: Int) {
+                if(which == 0){
+                    mainInterface.setLocal("vi")
+                }
+                if(which == 1){
+                    mainInterface.setLocal("en")
+                }
+                dialog?.dismiss()
+            }
+        })
+        val dialog = builder.create()
+        dialog.show()
     }
 
     companion object {
